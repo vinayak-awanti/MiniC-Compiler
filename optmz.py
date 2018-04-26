4d513c63e634c167b8444ec7e1523874c3b36dc5
root <vinayakawanti@gmail.com>
2018-04-26 18:47:05 +0530

    opt

--- optmz.py
+++ optmz.py
@@ -11,26 +11,28 @@ def func1(e):
 		return e.replace("1 *", "")
 	if('* 1' in e):
 		return e.replace("* 1", "")
-	if('/ 1' in e):
+	if('/ 1' == e[-2:]):
 		return e.replace("/ 1", "")
 	if('/ 0' in e):
 		print('Divide by Zero')
 		quit()	
 	try:
-		return eval(e)
+		return int(eval(e))
 	except NameError:
 		return 'NO'
 
 
 def constant_folding(input_file , output_file):
 	sys.stdout = open(output_file,'wt')
-	with open('ic') as fp:
+	with open(input_file) as fp:
 		for s in fp:
 			s = s.replace('\n','')
 			if('=' in s ):
 				a, b = s.split('=')
+				a, b = a.strip(' '), b.strip(' ')
 				x = func1(b)
-				if(x!='NO'):
+				str(x).strip(' ')
+				if(x!='NO' ):
 					print(a,'=',x)
 				else:
 					print(s)	
@@ -50,9 +52,10 @@ def constant_propagation(input_file , output_file):
 				a, b = a.strip(' '), b.strip(' ')
 				for i  in [*d]:
 					if(i in b):
-						if(i == b):
+						if(i == b and 't' in i):
 							out.remove(i+' = '+d[i])
-						b = b.replace(i,d[i])
+						if('<' not in b and '>' not in b and 'not' not in b):
+							b = b.replace(i,d[i])
 						
 						 
 				d[a] = b
@@ -64,3 +67,10 @@ def constant_propagation(input_file , output_file):
 	for i in out:
 		print(i)
 
+
+
+constant_folding('ic' , 'folding.txt')
+constant_propagation('folding.txt' ,'propagation.txt')
+constant_folding('propagation.txt' ,'final.txt')
+constant_propagation('final.txt' ,'final.txt')
+constant_folding('final.txt' ,'final.txt')
--- test.c
+++ test.c
@@ -5,34 +5,44 @@ int a = 10;
 /*this is a test program*/
 
 int main() {
-/*	hello world*/
-
-	while (a < 2) {
-		if (2<3) {
-			int z= 20;
-			z = 30;
-			a += 50;
-		} 
-		else {
-			int y =10;	
-			y++;
-		}
-	}
-	a /= 10;
-	a--;
 	
+	/*while loop*/
+	/*if else*/
+	/*scope*/
+	/*constant folding*/
+	/*constant propagation*/
+	/*array declaration*/
 	
-
+	int a , b;
+	a = 34 * 2 - 4;
+	b = a;
+	int i = 0;
+	
 	int arr[10], barr[10][10];
 	{
 		int *p, a, *ptr;
 	}
+
+	while (a < 2) {
+		while (b < 2){
+			i++;
+		}
+	}
 	
 	if(1<2){
-		int inif;
+		int inif = 0;
 	}
 	else {
 		int inelse = 0;
 	}
+
+	a /= 10;
+	a--;
+	
 	return 0;
 }
+
+/*
+PES1201702377
+chinras72
+*/
